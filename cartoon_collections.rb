13efea790a1822d6fb42b2c3e0e3725c7dfa08c6
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-01-27 15:59:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def long_planeteer_calls(words)
   words.any?{|word| word.size > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheeses)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheeses.include?(cheese_types)
 end
