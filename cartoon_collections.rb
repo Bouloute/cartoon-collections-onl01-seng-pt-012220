d8ba59011782ba5bdba9973611c3cee5fd28722c
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-01-27 16:11:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,5 +16,5 @@ end
 
 def find_the_cheese(cheeses)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheeses.select{|cheese| cheese_types.include?(cheese)}.to_s
+  cheeses.find{|cheese| cheese_types.include?(cheese)}
 end
